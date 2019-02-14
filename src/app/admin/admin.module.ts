289ab3838efb5a404f6d260bda481526de04387c
kisinga <stevekisinga@gmail.com>
2019-02-14 22:44:46 +0300

    deleted irrelevant pages

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,20 +1,17 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AdminRoutingModule} from './admin-routing.module';
-// import { AdminLayoutComponent } from './adminlayout.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {UsersGuard} from './guards/users.guard';
 import {RouterModule} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {LayoutModule} from './layout/layout.module';
-import {SampleModule} from '../main/sample/sample.module';
 import {AdminlayoutComponent} from './adminlayout.component';
 import {DocumentationModule} from './documentation/documentation.module';
 import {AuthenticationModule} from './authentication/authentication.module';
 import {CalendarModule} from './calendar/calendar.module';
 import {AdminSharedModule} from './shared/admin-shared.module';
-import { AdminprofileComponent } from './adminprofile/adminprofile.component';
-import {AgmCoreModule, AgmMap} from '@agm/core';
+import {AdminprofileComponent} from './adminprofile/adminprofile.component';
 
 @NgModule({
     imports: [
@@ -24,7 +21,6 @@ import {AgmCoreModule, AgmMap} from '@agm/core';
         LayoutModule,
 
         // App modules
-        SampleModule,
 
         RouterModule,
         FormsModule,

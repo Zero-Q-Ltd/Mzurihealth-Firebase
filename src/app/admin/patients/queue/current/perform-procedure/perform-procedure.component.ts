2e5334e9059d6ef13c8133c9c2168e945f14b1ed
kisinga <kisinga@zero-q.com>
2019-04-01 22:03:43 +0300

    code cleanup

---
+++ src/app/admin/patients/queue/current/perform-procedure/perform-procedure.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-perform-procedure',
+    templateUrl: './perform-procedure.component.html',
+    styleUrls: ['./perform-procedure.component.scss']
+})
+export class PerformProcedureComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}

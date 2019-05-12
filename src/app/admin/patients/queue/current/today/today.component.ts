898f90682fb755db5c1ced228915213ace66041b
kisinga <kamana@kisinga.family>
2019-05-12 07:14:36 +0300

    fixed bugs caused by using const

--- src/app/admin/patients/queue/current/today/today.component.ts
+++ src/app/admin/patients/queue/current/today/today.component.ts
@@ -203,13 +203,13 @@ export class TodayComponent implements OnInit {
      * initializes forms and from controls
      */
     initprocedureform(): void {
-        const results = new FormControl('', [Validators.required]);
-        const notes = new FormControl('', [Validators.required]);
+        let results = new FormControl('', [Validators.required]);
+        let notes = new FormControl('', [Validators.required]);
         this.procedureperformed = new FormGroup({
             results: results,
             notes: notes
         });
-        const selection = new FormControl('');
+        let selection = new FormControl('');
 
         this.procedureselection = new FormGroup({
             selection: selection,
@@ -222,12 +222,12 @@ export class TodayComponent implements OnInit {
      * @param mzio
      */
     igamizio(mzio): FormGroup {
-        const allergy = new FormControl({
+        let allergy = new FormControl({
             value: mzio.type,
             disabled: false
         });
 
-        const detail = new FormControl({
+        let detail = new FormControl({
             value: mzio.detail,
             disabled: false
         });
@@ -243,12 +243,12 @@ export class TodayComponent implements OnInit {
      * @param tatizo
      */
     igamatatizo(tatizo): FormGroup {
-        const allergy = new FormControl({
+        let allergy = new FormControl({
             value: tatizo.type,
             disabled: false
         });
 
-        const detail = new FormControl({
+        let detail = new FormControl({
             value: tatizo.detail,
             disabled: false
         });
@@ -260,9 +260,9 @@ export class TodayComponent implements OnInit {
     }
 
     createallergies(): FormGroup {
-        const allergy = new FormControl('');
+        let allergy = new FormControl('');
 
-        const detail = new FormControl({
+        let detail = new FormControl({
             value: '',
             disabled: true
         });
@@ -274,9 +274,9 @@ export class TodayComponent implements OnInit {
     }
 
     createmedconditions(): FormGroup {
-        const condition = new FormControl('');
+        let condition = new FormControl('');
 
-        const detail = new FormControl({
+        let detail = new FormControl({
             value: '',
             disabled: true
         });
